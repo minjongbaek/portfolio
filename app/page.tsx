e8bd744f2b12f7fa7f31715cb4bf708622d9b9b9
minjongbaek <minjongbaek@gmail.com>
2025-03-12 22:39:43 +0900

    feat: 이력서 기본 틀 작성

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,105 @@
-import Image from "next/image";
+const BIO = [
+  {
+    label: "Email",
+    href: "mailto:minjongbaek@gmail.com",
+  },
+  {
+    label: "GitHub",
+    href: "https://github.com/minjongbaek",
+  },
+  {
+    label: "Blog",
+    href: "https://blog.minjong.dev",
+  },
+];
 
-export default function Home() {
+const WORK_EXPERIENCE = [
+  {
+    id: "knowck",
+    title: "노크",
+    position: "풀스택 개발자",
+    startDate: "2019.12",
+    endDate: "2022.07",
+  },
+  {
+    id: "futurewiz",
+    title: "퓨쳐위즈",
+    position: "프론트엔드 개발자",
+    startDate: "2023.08",
+  },
+];
+
+const Resume = async () => {
+  const { default: IntroduceContent } = await import(`@/content/introduce.mdx`);
+
+  const workExperienceWithContent = (
+    await Promise.all(
+      [...WORK_EXPERIENCE].map(async (experience) => {
+        const { default: WorkExperienceContent } = await import(`@/content/experience/${experience.id}.mdx`);
+        return { ...experience, WorkExperienceContent };
+      })
+    )
+  ).reverse();
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="flex flex-col gap-20 mt-12 p-4">
+      <div className="flex flex-col gap-8">
+        <h1 className="leading-14">
+          안녕하세요,
+          <br />
+          프론트엔드 개발자 <span className="text-blue-500">백민종</span>입니다.
+        </h1>
+        <div className="flex gap-4">
+          {BIO.map(({ label, href }) => (
+            <a key={label} href={href} target="_blank">
+              {label}
+            </a>
+          ))}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div>
+          <IntroduceContent />
+        </div>
+      </div>
+      <div>
+        <h2>경력</h2>
+        <hr className="border-gray-300 mt-4 mb-8" />
+        <div className="flex flex-col gap-6">
+          {workExperienceWithContent.map(({ id, title, position, startDate, endDate, WorkExperienceContent }) => (
+            <div className="flex" key={id}>
+              <div className="flex flex-col gap-2 w-42 mr-6 shrink-0">
+                <h3>{title}</h3>
+                <div className="flex flex-col">
+                  <span>{position}</span>
+                  <span>
+                    {startDate} - {endDate ?? "재직중"}
+                  </span>
+                  <span>({getDuration(startDate, endDate)})</span>
+                </div>
+              </div>
+              <div className="border-l pl-6 border-gray-200/80 grow markdown">
+                <WorkExperienceContent />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
-}
+};
+
+export default Resume;
+
+const getDuration = (startDate: string, endDate?: string): string => {
+  const start = new Date(startDate);
+  const end = endDate ? new Date(endDate) : new Date();
+
+  let years = end.getFullYear() - start.getFullYear();
+  let months = end.getMonth() - start.getMonth();
+
+  if (months < 0) {
+    years -= 1;
+    months += 12;
+  }
+
+  return `${years}년 ${months}개월`;
+};
