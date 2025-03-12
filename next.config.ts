19a0b7b6484cdd4009eaf9467d5a9b72b1f33ea9
minjongbaek <minjongbaek@gmail.com>
2025-03-12 22:38:52 +0900

    feat: 이력서 기본 틀 작성

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
+import createMDX from "@next/mdx";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    turbo: {
+      resolveExtensions: [
+        ".mdx",
+        ".tsx",
+        ".ts",
+        ".jsx",
+        ".js",
+        ".mjs",
+        ".json",
+      ],
+    },
+  },
 };
 
-export default nextConfig;
+const withMdx = createMDX({});
+
+export default withMdx(nextConfig);
