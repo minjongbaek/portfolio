90dfaf099df1184b93b4d55f93b492d71b0b394f
minjongbaek <minjongbaek@gmail.com>
2025-04-10 21:40:31 +0900

    fix: Section 타이틀의 폰트가 작게 나오는 현상 수정

--- app/components/Section.tsx
+++ app/components/Section.tsx
@@ -4,7 +4,7 @@ import { SectionProps } from "@/types";
 export const Section: FC<SectionProps> = ({ title, isBreak, children }) => {
   return (
     <div className={isBreak ? "break-after-page" : ""}>
-      <h2 className="mb-4 text-2xl font-bold">{title}</h2>
+      <h2 className="mb-4">{title}</h2>
       <hr className="mt-4 mb-8 border-gray-300" />
       {children}
     </div>
